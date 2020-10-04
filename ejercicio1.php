5402abca70100e687ce69157a6a64716526e0542
kdtapias05 <karentapias.12@gmail.com>
2020-10-04 18:18:03 -0500

    Se crean los demas puntos

--- ejercicio1.php
+++ ejercicio1.php
@@ -3,15 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ejercicio2</title>
+    <title>ejercicio1</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
-
 <body>
 
-<HEADER>
-
-
+<header>
 
 <nav>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -41,87 +38,91 @@
        </form>
        </div>
 
-    
 </nav>
 
-</HEADER>
 
-<MAIN>
+</header>
 
-    
-    <div class="container-fluid">
-        <h1 class="text-center mt-3"><img src="descarga.png" alt="Imagen1" width = "400px"></h1>
-        <h4 class="text-center">Indice de Masa Corporal (IMC)</h4>
-        <form class="mt-3" action="ejercicio2.php" method="POST">
+<main>
+
+<div class="container-fluid">
+        <h1 class="text-center">CÁLCULOS ENTRE 2 NÚMEROS</h1>
+        <form action="ejercicio1.php" method="POST">
             <div class="form-group row mt-3 justify-content-center">
-                <label for="peso">Peso</label>
-             <div class="col-2">
-                <input type="number" name="peso" id="peso" class="form-control"  placeholder="Peso">
+                <label for="number1" class="col-1">Número 1</label>
+                <input type="number" name="number1" id="number1" class="form-control col-2"  placeholder="Número 1">
+                <label for="number2" class="col-1">Número 2</label>
+                <input type="number" class="form-control col-2" name="number2" id="number2" placeholder="Número 2">
             </div>
-                <label for="Altura" class="">Altura</label>
-            <div class="col-2">
-                    <input value="" type="number" name="altura" id="altura" step="any" class="form-control"  placeholder="Altura" >
+            <div class="form-group row mt-3 justify-content-center">
+                <select name="operator" id="operator" class="form-control offset-1 col-5">
+                    <option value="" selected disabled>Selecciona una operacion</option>
+                    <option value="add">Sumar</option>
+                    <option value="rest">Restar</option>
+                    <option value="multiplication">Multiplicar</option>
+                    <option value="divide">Dividir</option>
+                </select>
             </div>
-    </div>
-    <div class="form-group row justify-content-center">
-        <div class="col-1">
-            <button type="submit" name="botonCalcular" class="btn btn-dark mt-4 btn-block">Calcular</button>
-        </div>
-    </div>
+            
+            <div class="form-group row justify-content-center">
+                <div class="col-1">
+                    <button type="submit" name ="calcular" class="btn btn-primary">Calcular</button>
+                </div>
+            </div>
         </form>
-
-        <?php if(isset($_POST["botonCalcular"])): ?>
         
-             <?php 
-                $peso=$_POST["peso"];   
-                $altura=$_POST["altura"];
-                $imc= round($peso/($altura * $altura),1);
+        <?php if(isset($_POST['calcular'])) : ?>
+        
+        <?php 
+            if(!empty($_POST['operator'])) {
+                $operator = $_POST['operator'];
+                
+                $number1 = isset($_POST['number1']);
+                $number2 = isset($_POST['number2']);
+                
+                if ($operator == "add") {
+                    
+                    $result = $_POST["number1"] + $_POST["number2"];
+                    
+                }
+                else if ($operator == "rest") {
+                    $result = $_POST["number1"] - $_POST["number2"];
+                }
+                else if ($operator == "multiplication") {
+                    $result = $_POST["number1"] * $_POST["number2"];
+                }
+                else {
+                    $result = $_POST["number1"] / $_POST["number2"];
+                }
+            }
+            else {
+                echo 'No se ha seleccionado ningún operador';     
+            }
+        ?>
+                
+                <h4 class="text-center">
+                    <?php echo("El resultado de la operación es: ". $result) ?>
+                </h4>
 
-                if ($imc<18.5) { 
-                    $escala="Peso insuficiente";
-                } 
-                else if (($imc>=18.5)&&($imc<=24.9)){ 
-                    $escala="Normopeso";
-                }
-                else if (($imc>=25)&&($imc<=26.9)){ 
-                    $escala="Sobrepeso grado I";
-                }
-                else if (($imc>=27)&&($imc<=29.9)){
-                    $escala="Sobrepreso grado II (preobesidad)";
-                }
-                else if (($imc>=30)&&($imc<=34.9)){ 
-                    $escala="Obesidad de tipo I";
-                }
-                else if (($imc>=35)&&($imc<=39.9)){
-                    $escala="Obesidad de tipo II";
-                }
-                else if (($imc>=40)&&($imc<=49.9) ){ 
-                    $escala="Obesidad de tipo III (mórbida)";
-                }
-                else{ 
-                    $escala="Obesidad de tipo IV (extrema)";
-                }
-            ?>
-            <br>
-            <h4 class="text-center">
-                <?php 
-                 echo ("Tu IMC es: ".$imc);
-                 echo ("<br>");
-                 echo ("Escala: ".$escala); 
-                ?>
-            </h4>
-            <?php endif ?>
+                    <h4></h4>
+                <?php endif ?>
 
-</MAIN>
+   
+    </div>
 
-<FOOter>
+</main>
 
-</FOOter>
+<footer>
 
+
+</footer>
+    
+    
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
 </body>
-</html>
+
+</html> 
---
+++ ejercicio4.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejercicio2</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
+
+<HEADER>
+
+<nav>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">WEB 1</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+        <a class="nav-link" href="ejercicio1.php">Punto 1 <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+        <a class="nav-link" href="ejercicio2.php">Punto 2 <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+        <a class="nav-link" href="ejercicio3.php">Punto 3 <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+        <a class="nav-link" href="ejercicio4.php">Punto 4 <span class="sr-only">(current)</span></a>
+        </li>
+        </ul>
+       <form class="form-inline my-2 my-lg-0">
+         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+       </form>
+       </div>
+</nav>
+
+</HEADER>
+
+<MAIN>
+<div class="container-fluid">
+<h1 class="text-center mt-3"><img src="logo-bancolombia.png" alt="Imagen1" width = "400px"></h1>
+<h4 class="text-center">EMPLEADOS BANCOLOMBIA</h4>
+<form action="ejercicio4.php" method="POST">
+<div class="form-group row mt-3 justify-content-center">
+        <h6 class="col-12">Ingrese la información del empleado 1</h6>
+        <label for="name1" class="col-1">Nombre</label>
+        <input type="text" name="name1" id="name1" class="form-control col-2"  placeholder="Nombre">
+        <label for="phone1" class="col-1">Teléfono</label>
+        <input type="text" name="phone1" id="phone1" class="form-control col-1"  placeholder="Teléfono">
+        <label for="direction1" class="col-1">Dirección</label>
+        <input type="text" name="direction1" id="direction1" class="form-control col-3"  placeholder="Dirección">
+        <label for="salary1" class="col-1">Salario</label>
+        <input type="number" class="form-control col-1" name="salary1" id="salary1" placeholder="Salario">
+</div>
+<div class="form-group row mt-3 justify-content-center">
+        <h6 class="col-12">Ingrese la información del empleado 2</h6>
+        <label for="name2" class="col-1">Nombre</label>
+        <input type="text" name="name2" id="name2" class="form-control col-2"  placeholder="Nombre">
+        <label for="phone2" class="col-1">Teléfono</label>
+        <input type="text" name="phone2" id="phone2" class="form-control col-1"  placeholder="Teléfono">
+        <label for="direction2" class="col-1">Dirección</label>
+        <input type="text" name="direction2" id="direction2" class="form-control col-3"  placeholder="Dirección">
+        <label for="salary2" class="col-1">Salario</label>
+        <input type="number" name="salary2" id="salary2" class="form-control col-1" placeholder="Salario">
+</div>
+<div class="form-group row mt-3 justify-content-center">
+        <h6 class="col-12">Ingrese la información del empleado 3</h6>
+        <label for="name3" class="col-1">Nombre</label>
+        <input type="text" name="name3" id="name3" class="form-control col-2"  placeholder="Nombre">
+        <label for="phone3" class="col-1">Teléfono</label>
+        <input type="text" name="phone3" id="phone3" class="form-control col-1"  placeholder="Teléfono">
+        <label for="direction3" class="col-1">Dirección</label>
+        <input type="text" name="direction3" id="direction3" class="form-control col-3"  placeholder="Dirección">
+        <label for="salary3" class="col-1">Salario</label>
+        <input type="number" name="salary3" id="salary3" class="form-control col-1"  placeholder="Salario">
+</div>
+<div class="form-group row mt-3 justify-content-center">
+        <h6 class="col-12">Ingrese la información del empleado 4</h6>
+        <label for="name4" class="col-1">Nombre</label>
+        <input type="text" name="name4" id="name4" class="form-control col-2"  placeholder="Nombre">
+        <label for="phone4" class="col-1">Teléfono</label>
+        <input type="text" name="phone4" id="phone4" class="form-control col-1"  placeholder="Teléfono">
+        <label for="direction4" class="col-1">Dirección</label>
+        <input type="text" name="direction4" id="direction4" class="form-control col-3"  placeholder="Dirección">
+        <label for="salary4" class="col-1">Salario</label>
+        <input type="number" name="salary4" id="salary4" class="form-control col-1" placeholder="Salario">
+</div>
+<div class="form-group row mt-3 justify-content-center">
+        <h6 class="col-12">Ingrese la información del empleado 5</h6>
+        <label for="name5" class="col-1">Nombre</label>
+        <input type="text" name="name5" id="name5" class="form-control col-2"  placeholder="Nombre">
+        <label for="phone5" class="col-1">Teléfono</label>
+        <input type="text" name="phone5" id="phone5" class="form-control col-1"  placeholder="Teléfono">
+        <label for="direction5" class="col-1">Dirección</label>
+        <input type="text" name="direction5" id="direction5" class="form-control col-3"  placeholder="Dirección">
+        <label for="salary5" class="col-1">Salario</label>
+        <input type="number" name="salary5" id="salary5" class="form-control col-1" placeholder="Salario">
+</div>
+            
+            
+<div class="form-group row justify-content-center">
+<div class="col-1">
+        <button type="submit" name ="calculate" class="btn btn-dark">CALCULAR</button>
+</div>
+</div>
+</form>
+
+        <?php if(isset($_POST["send"])) :?>
+        <?php
+            $salaryB = 40000000;
+            $salaryC = 32000000;
+            $mayorSucursal;
+            $names = array($_POST["name1"],$_POST["name2"],$_POST["name3"],$_POST["name4"],$_POST["name5"]);
+            $phones = array($_POST["phone1"],$_POST["phone2"],$_POST["phone3"],$_POST["phone4"],$_POST["phone5"]);
+            $directions = array($_POST["direction1"],$_POST["direction2"],$_POST["direction3"],$_POST["direction4"],$_POST["direction5"]);
+            $salarys = array($_POST["salary1"],$_POST["salary2"],$_POST["salary3"],$_POST["salary4"],$_POST["salary5"]);
+
+            $sumatoriaSalarios=$salarys[0] + $salarys[1] + $salarys[2] + $salarys[3] + $salarys[4];
+
+            ?>
+        <?php 
+            if($sumatoriaSalarios > $salaryB) {
+                $mayorSucursal = "A";
+            } 
+            else {
+                $mayorSucursal = "B";
+            }
+        ?>    
+        <h2>La sucursal <?php echo($mayorSucursal);?> tiene la mejor sumatoria de salarios"</h2>
+        <br>
+        <br>
+        <h4><?php echo("Nombre del empleado: ". $names[0]." Teléfono: ".$phones[0]." Dirección: ".$directions[0]." Salario: ". $salarys[0] ); ?></h4>
+        <h4><?php echo("Nombre del empleado: ". $names[1]." Teléfono: ".$phones[1]." Dirección: ".$directions[1]." Salario: ". $salarys[1] ); ?></h4>
+        <h4><?php echo("Nombre del empleado: ". $names[2]." Teléfono: ".$phones[2]." Dirección: ".$directions[2]." Salario: ". $salarys[2] ); ?></h4>
+        <h4><?php echo("Nombre del empleado: ". $names[3]." Teléfono: ".$phones[3]." Dirección: ".$directions[3]." Salario: ". $salarys[3] ); ?></h4>
+        <h4><?php echo("Nombre del empleado: ". $names[4]." Teléfono: ".$phones[4]." Dirección: ".$directions[4]." Salario: ". $salarys[4] ); ?></h4>
+                
+        <?php endif?>
+
+    </div>
+    
+
+
+
+</MAIN>
+
+<FOOter>
+
+</FOOter>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>   
+
+</body>
+</html>
